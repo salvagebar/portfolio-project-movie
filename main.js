52835d0047818dfee606fb40e1e31da01a85865a
salvagebar <salvagebar@gmail.com>
2023-04-02 16:25:33 -0400

    Added basic grid layout, initial API call for favorite movie

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 const url = "http://www.omdbapi.com/?t=Casablanca&apikey=82153485";
-const p = document.querySelector(".fav-Pokemon");
+const favoriteMovie = document.querySelector(".fav-movie");
 
 fetch(url)
   .then((response) => response.json())
   .then((data) => {
-    p.innerHTML = `My favorite movie is ${data.Title}!<br> <img src="${data.Poster}" alt="${data.Title}">`
+    favoriteMovie.innerHTML = `<p>My favorite movie is ${data.Title}!</p> <img src="${data.Poster}" alt="${data.Title}">`;
     console.log(data);
   });
