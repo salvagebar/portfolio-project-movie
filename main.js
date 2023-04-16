b2e3b40d3f50f33179f5607c03d350347a960251
salvagebar <salvagebar@gmail.com>
2023-04-16 15:40:29 -0400

    Added HTML for form and results

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
-const url = "http://www.omdbapi.com/?t=Casablanca&apikey=82153485";
-const favoriteMovie = document.querySelector(".fav-movie");
+const faveUrl = "http://www.omdbapi.com/?t=Casablanca&apikey=82153485"; // url of Casablanca, the best movie
+const favoriteMovie = document.querySelector(".fav-movie"); // where the Casablanca info & poster go
+const searchedMovieInfo = document.getElementById("movie-info"); // where the user's movie info goes
+const searchedMoviePoster = document.getElementById("movie-poster"); // where the user's movie poster goes
+const movieSearch = document.getElementById("movie-input"); // the search form
 
-fetch(url)
+fetch(faveUrl)
   .then((response) => response.json())
   .then((data) => {
     favoriteMovie.innerHTML = `<p>My favorite movie is ${data.Title}!</p> <img src="${data.Poster}" alt="${data.Title}">`;
